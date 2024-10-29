4f436f228b5757b79514b071d68c87fc8411acfe
zoleg-oi <zoleg@mail.ru>
2024-10-29 22:18:14 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,10 @@
+# Генераторные сборки
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(x[0]) - len(x[1]) for x in zip(first, second) if len(x[0]) != len(x[1]))
+second_result = (True if len(first[x]) == len(second[x]) else False for x in range(len(first)))
+
+print(list(first_result))
+print(list(second_result))
