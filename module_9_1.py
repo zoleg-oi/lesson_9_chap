37d01c39ab18b07a2deedd870e2a4121e84fa451
zoleg-oi <zoleg@mail.ru>
2024-10-29 20:06:14 +0300

    Add files via upload

--- module_9_1.py
+++ module_9_1.py
@@ -3,9 +3,13 @@
 def apply_all_func(int_list, *functions):
     results = {}
     for fun in functions:
-        results[fun.__name__] = fun(int_list)
+        try:
+            results[fun.__name__] = fun(int_list)
+        except TypeError:
+            results[fun.__name__] = "Операции возьожны только с числами"
+
     return results
 
-print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, "f", 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
 
