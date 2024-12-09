3a4b99f2eda6bf28e691e7c29ce6b9742bdde801
zoleg-oi <zoleg@mail.ru>
2024-12-09 21:31:35 +0300

    Add files via upload

--- module_9_6_2.py
+++ module_9_6_2.py
@@ -1,13 +1,14 @@
 # Генераторы
 
 def all_variants(text):
-
     for i in range(len(text)):
         t = text[i]
         yield t
-        for ii in range(i+1,len(text)):
+        for ii in range(i + 1, len(text)):
             t += text[ii]
             yield t
+
+
 a = all_variants('abc')
 
 print(a)
